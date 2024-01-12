c2b6b7948a66e0f8a72f97f57a51ce39bf7ea2e9
AzurPanto <pallarikhil@gmail.com>
2024-01-12 20:40:33 +0100

    first commit

---
+++ bot.js
@@ -0,0 +1,38 @@
+const { Client, IntentsBitField, ActivityType } = require("discord.js");
+
+require("dotenv").config();
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const DISCORD_TOKEN = (process.env.TOKEN);
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.on("ready", () => {
+  console.log("IS READY TO RUMBLE!");
+  console.log(`${client.user.tag} IS RUMBLING`);
+
+  client.user.setActivity({
+    name: "Counting the Shits",
+    type: ActivityType.Playing,
+  });
+});
+
+client.on('messageCreate', message =>  {
+
+    if(message.content.toLowerCase() === 'hey there')
+        message.channel.send("What's up numbnuts");
+
+    
+
+});
+
+client.login(DISCORD_TOKEN);
